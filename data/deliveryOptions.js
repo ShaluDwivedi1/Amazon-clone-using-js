bf934b53df4290202733254b21ba4b835dfcb070
ShaluDwivedi1 <154000437+ShaluDwivedi1@users.noreply.github.com>
2026-03-24 12:28:15 +0530

    Add files via upload

---
+++ data/deliveryOptions.js
@@ -0,0 +1,25 @@
+export const deliveryOptions=[{
+  id:'1',
+  deliveryDays:7,
+  pricePaise:0
+},{
+  id:'2',
+  deliveryDays:3,
+  pricePaise:4009
+},{
+  id:'3',
+  deliveryDays:1,
+  pricePaise:9090
+}];
+
+export function getDeliveryOption(deliveryOptionId){
+  let deliveryOption;
+
+  deliveryOptions.forEach((option)=>{
+    if(option.id === deliveryOptionId){
+      deliveryOption = option;
+    }
+  });
+
+  return deliveryOption || deliveryOptions[0];
+}
